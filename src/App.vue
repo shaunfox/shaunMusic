e9ec5d40f0e4f49bbc3a60996238777071cf0196
Shaun Wang <shaun.wang@hotmail.com>
2017-08-12 14:16:47 +0800

    init

---
+++ src/App.vue
@@ -0,0 +1,17 @@
+<template>
+  <div id="app">
+    <m-header></m-header>
+  </div>
+</template>
+
+<script>
+import MHeader from 'components/m-header/m-header'
+export default {
+  components: {
+    MHeader
+  }
+}
+</script>
+
+<style>
+</style>
